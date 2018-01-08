cfd26975d2438bab7bec920f4fa89a9fe9d3f197
Alejandro Castello <a.castello.pas@gmail.com>
2018-01-08 19:16:04 +0100

    .

---
+++ src/UI/MCurses.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE FlexibleContexts, RankNTypes #-}
+
+module MCurses where
+
+import Control.Monad
+import Control.Monad.State
+
+import UI.NCurses
+
+data MEnv = MEnv 
+  { menv_x :: Int }
+
+
+type MCurses m a = (Monad m, MonadState MEnv m) => m a
+x :: MonadState MEnv m => m Int
+x = menv_x <$> get
