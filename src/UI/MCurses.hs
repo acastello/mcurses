c19eae7486944bcc7d486bfb00c28fa63c25dac4
Alejandro Castello <a.castello.pas@gmail.com>
2018-07-27 01:33:15 +0200

    .

--- src/UI/MCurses.hs
+++ src/UI/MCurses.hs
@@ -1,12 +1,22 @@
-{-# LANGUAGE FlexibleContexts, RankNTypes, FlexibleInstances, MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts, RankNTypes, FlexibleInstances, 
+    MultiParamTypeClasses, CApiFFI #-}
 
-module MCurses where
+-- module MCurses where
 
 import qualified Data.Map as M
 import Control.Monad
 import Control.Monad.State
 
+import Foreign
+import Foreign.C
+
 import UI.NCurses
+
+foreign import capi "curses.h initscr" initscr :: IO ()
+foreign import capi "curses.h value stdscr" stdscr :: WindowPtr
+
+type WindowPtr = Ptr ()
+-- newtype {-# CTYPE "curses.h" "WINDOW *" #-} WindowPtr = WindowPtr (Ptr WindowPtr)
 
 data MEnv m s = MEnv 
   { me_panels :: M.Map String (Panel m s) 
@@ -28,3 +38,8 @@ f :: MCurses m s [Panel m s]
 f = me_z <$> get
 
 -- instance MonadState (MEnv m s, s) m => MonadState s m where
+
+main :: IO ()
+main = do
+    initscr
+    print stdscr
