74864a099bcf029d68ac1a660b91ac0fbc5f5970
Alejandro Castelló <a.castello.pas@gmail.com>
2018-09-17 18:14:45 +0200

    cleanup and commenting out

--- src/UI/FRP/MCurses.hs
+++ src/UI/FRP/MCurses.hs
@@ -39,157 +39,38 @@ type SignalSource val = IOMC (Maybe (SignalTup val))
 type SignalTup val = (SignalCell val, Signal val)
 data SignalCell val = ConstCell val | InitCell Unique (IOMC ())
 
-instance Semigroup (Signal val) where
-    (<>) = mappend
-
-instance Monoid (Signal val) where
-    mempty = Signal (return Nothing)
-    mappend = attach
-
-
-instance Functor Signal where
-    fmap = mapSignal
-
-instance Applicative Signal where
-    pure x = Signal (return (Just (ConstCell x, mempty)))
-
--- mapSignal :: (a -> IOMC b) -> Signal a -> Signal b
--- mapSignal f (Signal op) = Signal $ op >>= mapM
---     (\(x, ml) -> do y <- f x
---                     return (y, mapSignal f ml))
+-- window :: Int -> Curs IO (Signal ByteString) (Signal ByteString)
+-- window n = (\s -> consum s =<< iniW) ^>> act 
+--     where 
+--     iniW = do 
+--         y <- liftIO (randomRIO (0.2,0.8))
+--         x <- liftIO (randomRIO (0.2,0.8))
+--         w <- newWindow (Height/5) (Width/5) 
+--                        (Absolute y * Height) (Absolute x * Width)
+--         drawBorder w
+--         render
+--         return w
+--     consum sig win = mapSignalWithInit (\str -> return (rend win str, 
+--                                                         tran win str)) sig
+--     rend win str = do 
+--         erase win
+--         drawBorder win
+--         moveCursor win 1 1
+--         drawByteString win str
+--         render
+--         liftIO $ threadDelay (n * 100000)
+--     tran _ str = do 
+--         return (BS.drop 1 str)
 -- 
-mapCells :: (SignalCell a -> IOMC (SignalCell b)) -> Signal a -> Signal b
-mapCells f (Signal op) = Signal $ op >>= mapM trans
-    where
-    trans (c, sig) = f c >>= \c' -> return (c', mapCells f sig)
+-- m1 :: Curs IO () [ByteString]
+-- m1 = proc () -> do
+--     rec os <- signaler 4 -< pure "string" <> is
+--         let is = mapMSignal (\x -> liftIO (randomRIO ('A','z')) >>= \c -> return (x <> BS.singleton c)) os
+--     act -< runSig (os `until` ((== 0) . BS.length))
 
-mapSignal :: (a -> b) -> Signal a -> Signal b
-mapSignal f s = mapMSignal (return . f) s
-
-mapMSignal :: (a -> IOMC b) -> Signal a -> Signal b
-mapMSignal f = mapCells g
-    where
-    g (ConstCell x) = ConstCell `liftM` f x
-    g (InitCell u op) = return (InitCell u op)
-
-mapSignalWithInit :: (a -> IOMC (IOMC (), IOMC b)) -> Signal a -> IOMC (Signal b)
-mapSignalWithInit genOp sig = do
-    u <- liftIO newUnique 
-    return (map' u sig)
-    where
-    map' u (Signal op) = Signal (op >>= trans u)
-    trans _ Nothing = return Nothing
-    trans u (Just (ConstCell x, nextSig)) = do 
-        (ini, f) <- genOp x
-        return $ Just $ (,) (InitCell u ini) $ Signal $ do
-            y <- f 
-            return $ Just (ConstCell y, map' u nextSig)
-    trans u (Just (InitCell u' ini, sigNext @ (Signal op'))) 
-        | u /= u' = return $ Just (InitCell u' ini, map' u sigNext)
-        | otherwise = op' >>= trans u 
-
-foreverSignal :: (IOMC a) -> Signal a
-foreverSignal op = Signal $ do 
-    x <- op
-    return $ Just (ConstCell x, foreverSignal op)
-
-cat :: a -> Signal a -> Signal a
-cat x sig = Signal (return (Just (ConstCell x, sig)))
-
-until :: Signal a -> (a -> Bool) -> Signal a
-until s f = untilM s (return . f)
-
-untilM :: Signal a -> (a -> IOMC Bool) -> Signal a
-untilM (Signal op) cmp = Signal $ op >>= \m -> case m of
-    Nothing -> return Nothing
-    Just (c @ InitCell {}, sig) -> return $ Just (c, untilM sig cmp)
-    Just (c @ (ConstCell x), sig) -> do 
-        b <- cmp x
-        return $ if b then Nothing 
-                      else Just (c, untilM sig cmp)
-
-zipSignals :: (a -> b -> c) -> Signal a -> Signal b -> Signal c
-zipSignals f s s' = zipMSignals (\x y -> return (f x y)) s s'
-
-zipMSignals :: (a -> b -> IOMC c) -> Signal a -> Signal b -> Signal c
-zipMSignals f (Signal op) (Signal op') = Signal $ do
-    mtup <- op
-    mtup' <- op'
-    mstep mtup mtup'
-    where
-    mstep Nothing _ = return Nothing
-    mstep _ Nothing = return Nothing
-    mstep (Just t) (Just t') = step t t'
-    step (InitCell u ini, s) (InitCell u' ini', s') = return $ Just $ 
-        (,) (InitCell u ini) $ Signal $ return $ Just $ 
-            (,) (InitCell u' ini') $ Signal $ do
-                unSignal (zipMSignals f s s')
-    step (ConstCell x, s) (ConstCell x', s') = do 
-        y <- f x x'
-        return $ Just (ConstCell y, zipMSignals f s s')
-    step (InitCell u ini, s) tup' = return $ Just $ 
-        (,) (InitCell u ini) $ Signal $ do
-            mtup <- unSignal s
-            mstep mtup (Just tup')
-    step tup (InitCell u ini, s) = return $ Just $ 
-        (,) (InitCell u ini) $ Signal $ do
-            mtup' <- unSignal s
-            mstep (Just tup) mtup'
-
-attach :: Signal a -> Signal a -> Signal a
-attach (Signal op) sig = Signal $ op >>= \m -> case m of
-    Nothing -> unSignal sig
-    Just (x, sig') -> return $ Just (x, attach sig' sig)
-
-runSig :: Signal a -> IOMC [a]
-runSig (Signal op) = op >>= maybe (return []) f
-    where
-    f (c, sig) = liftM2 (++) (f' c) (runSig sig) 
-    f' (ConstCell x) = return [x]
-    f' (InitCell _ ini) = ini >> return []
-
-debugSig :: Signal a -> IOMC [a]
-debugSig (Signal op) = op >>= maybe (return []) f
-    where
-    f (c, sig) = liftM2 (++) (f' c) (debugSig sig)
-    f' (ConstCell x) = liftIO (BS.hPutStrLn stderr "Const") >> return [x]
-    f' (InitCell _ ini) = liftIO (BS.hPutStrLn stderr "Init") >> ini >> return []
-
-signaler :: Int -> Curs IO (Signal ByteString) (Signal ByteString)
-signaler n = (\s -> consum s =<< iniW) ^>> act 
-    where 
-    iniW = do 
-        y <- liftIO (randomRIO (0.2,0.8))
-        x <- liftIO (randomRIO (0.2,0.8))
-        w <- newWindow (Height/5) (Width/5) 
-                       (Absolute y * Height) (Absolute x * Width)
-        drawBorder w
-        render
-        return w
-    consum sig win = mapSignalWithInit (\str -> return (rend win str, 
-                                                        tran win str)) sig
-    rend win str = do 
-        erase win
-        drawBorder win
-        moveCursor win 1 1
-        drawByteString win str
-        render
-        liftIO $ threadDelay (n * 100000)
-    tran _ str = do 
-        return (BS.drop 1 str)
-
-m1 :: Curs IO () [ByteString]
-m1 = proc () -> do
-    rec os <- signaler 4 -< pure "string" <> is
-        let is = mapMSignal (\x -> liftIO (randomRIO ('A','z')) >>= \c -> return (x <> BS.singleton c)) os
-    act -< runSig (os `until` ((== 0) . BS.length))
-
-input :: Curs IO () (Signal ByteString)
-input = arr (\() -> foreverSignal getByteString)
-
-runCurs :: (MonadIO m, MonadMask m) => Curs m a b -> a -> m b
-runCurs (Curs op) i = runMC $ do
-    runKleisli (runReader op) (i, foreverSignal waitInput)
+-- runCurs :: (MonadIO m, MonadMask m) => Curs m a b -> a -> m b
+-- runCurs (Curs op) i = runMC $ do
+--     runKleisli (runReader op) (i, foreverSignal waitInput)
 
 act :: Monad m => Curs m (MC m output) output
 act = Curs $ (A.lift) (Kleisli id)
