a9c74b99ef5b52489f0ff014ee5f404cbf8ec71f
Alejandro Castello <a.castello.pas@gmail.com>
2018-09-18 15:20:21 +0200

    first succesful aproach

--- src/UI/FRP/MCurses.hs
+++ src/UI/FRP/MCurses.hs
@@ -9,11 +9,14 @@ import Control.Category hiding ((.), id)
 import Control.Concurrent 
 import Control.Monad
 import Control.Monad.IO.Class
+import Control.Wire hiding (id, (.))
 
 import Data.ByteString.Char8 as BS
 import Data.List as L
 import Data.Semigroup
 import Data.Unique
+
+import FRP.Netwire hiding (id, (.))
 
 import Prelude hiding (until)
 
@@ -22,46 +25,25 @@ import System.Random
 
 import UI.MCurses hiding (io)
 
-newtype Curs m a b = Curs (InputsReader (MCArrow m) a b)
-    deriving (Functor, Applicative, Category, Arrow, ArrowChoice, ArrowApply, 
-              ArrowLoop)
+type Curs = Wire () () (MC IO) 
 
-type InputsReader = ReaderArrow (SignalSource Input)
-type MCArrow m = Kleisli (MC m)
+asd :: Curs String String
+asd = mkGen_ $ \str -> do
+    c <- liftIO $ randomRIO ('A','z')
+    return $ Right (c : str)
 
-type IOMC = MC IO
-type SignalSource = IOMC
-type Finalizer = IOMC ()
-
-data Signal a = Signal a (Wire a) Finalizer 
-
-newtype Wire a = Wire [a -> IOMC a]
-
-runSignal :: Signal a -> IOMC a
-runSignal (Signal sourc (Wire wires) fini) = do
-    x <- L.foldr (>=>) return wires sourc
-    fini
-    return x
-
-source :: a -> Signal a -> Signal a
-source x (Signal _ trans fini) = Signal x trans fini
-
-trans :: (a -> IOMC a) -> Signal a -> Signal a
-trans op (Signal ini (Wire ops) fini) = Signal ini (Wire $ ops ++ [op]) fini
-
-signaler :: Int -> Int -> Curs IO (Signal ByteString) (Signal ByteString)
-signaler delayn dropn = (\s -> consum s `liftM` iniW) ^>> act 
+signaler :: Int -> Int -> Curs ByteString ByteString
+signaler delayn dropn = mkGen startit
     where 
+    startit () str = do win <- iniW
+                        y <- repl win str
+                        return (y, mkGen_ $ repl win)
     iniW = do 
         y <- liftIO (randomRIO (0.2,0.8))
         x <- liftIO (randomRIO (0.2,0.8))
-        w <- newWindow (Height/4) (Width/4) 
-                       (Absolute y * Height) (Absolute x * Width)
-        drawBorder w
-        render
-        return w
-    consum sig win = trans (repl win) sig
-    repl win str = rend win str >> tran win str
+        newWindow (Height/4) (Width/4) 
+                  (Absolute y * Height) (Absolute x * Width)
+    repl win str = rend win str >> (Right `liftM` tran win str)
     rend win str = do 
         erase win
         drawBorder win
@@ -72,23 +54,23 @@ signaler delayn dropn = (\s -> consum s `liftM` iniW) ^>> act
     tran _ str = do 
         liftIO $ hPrint stderr $ "signaler: " <> str
         ch <- liftIO (randomRIO ('A','z'))
-        return (BS.drop dropn str <> BS.singleton ch)
+        return (BS.drop dropn $ str <> BS.singleton ch)
                                           
-m1 :: Curs IO () ByteString               
+m1 :: Curs () ByteString
 m1 = proc () -> do
-    rec os <- signaler 9 2 -< source "string strung" os
-    act -< runSignal os
+    rec 
+        is <- delay "string strung" <<< until -< (os, ev)
+        os <- signaler 3 2 -< is
+        ev <- noLonger ((> 0) . BS.length) -< os
+    returnA -< os
+
+runCurs :: Curs () a -> IO [a]
+runCurs = runMC . f
+    where
+    f wir = do (eith, wir') <- stepWire wir () (Right ())
+               case eith of Left _ -> return []
+                            Right x -> (x:) `liftM` f wir'
 
 -- input :: Curs IO () (Signal ByteString)
 -- input = arr (\() -> foreverSignal getByteString)
 -- 
-runCurs :: (MonadIO m, MonadMask m) => Curs m a b -> a -> m b
-runCurs (Curs op) i = runMC $ do
-    runKleisli (runReader op) (i, waitInput)
-
-act :: Monad m => Curs m (MC m output) output
-act = Curs $ (A.lift) (Kleisli id)
-
-actM :: Monad m => Curs m (m output) output
-actM = Curs $ (A.lift) (Kleisli lift)
-
