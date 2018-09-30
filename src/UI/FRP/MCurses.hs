fdd4eb597d581d8165978fc9f3e7b4e37b270c1a
Alejandro Castelló <a.castello.pas@gmail.com>
2018-09-30 12:55:34 +0200

    too similar to netwire atm

--- src/UI/FRP/MCurses.hs
+++ src/UI/FRP/MCurses.hs
@@ -1,94 +1,85 @@
-{-# LANGUAGE Arrows, GeneralizedNewtypeDeriving, GADTs, RankNTypes, ImpredicativeTypes #-}
+{-# LANGUAGE Arrows                       #-}
+{-# Language GeneralizedNewtypeDeriving   #-}
+{-# Language GADTs                        #-} 
+{-# Language RankNTypes                   #-}
+{-# Language ImpredicativeTypes           #-}
+{-# Language FlexibleInstances            #-}
 
 module UI.FRP.MCurses where
 
-import Control.Arrow
+import           Control.Arrow
 import qualified Control.Arrow.Transformer as A
-import Control.Arrow.Transformer.Reader
-import Control.Category hiding ((.), id)
-import Control.Concurrent 
-import Control.Monad
-import Control.Monad.IO.Class
+import           Control.Arrow.Transformer.Reader
+import qualified Control.Category
+import           Control.Category hiding ((.), id)
+import           Control.Concurrent 
+import           Control.Monad
+import           Control.Monad.Fix
+import           Control.Monad.IO.Class
 
-import Data.ByteString.Char8 as BS
-import Data.List as L
-import Data.Semigroup
-import Data.Unique
+import           Data.ByteString.Char8 hiding (putStrLn)
+import qualified Data.ByteString.Char8 as BS
+import           Data.List as L
+import           Data.Semigroup
+import           Data.Unique
 
-import Prelude hiding (until)
+import           Prelude hiding (until)
 
-import System.IO
-import System.Random
+import           System.IO
+import           System.Random
 
-import UI.MCurses hiding (io)
+import           UI.MCurses hiding (io)
 
-newtype Curs m a b = Curs (InputsReader (MCArrow m) a b)
-    deriving (Functor, Applicative, Category, Arrow, ArrowChoice, ArrowApply, 
-              ArrowLoop)
+data Signal m a b where
+    Id :: Signal m a a
+    Pure :: (a -> b) -> Signal m a b
+    Trans :: (a -> m b) -> Signal m a b
+    Loop :: Signal m (a, d) (b, d) -> Signal m a b
+    Gen :: m b -> Signal m a b
 
-type InputsReader = ReaderArrow (SignalSource Input)
-type MCArrow m = Kleisli (MC m)
+instance Category (Signal IO) where
+    id = Id
+    (.) = dot
+        where
+        Id       `dot` sig = sig
+        sig      `dot` Id  = sig
+        Pure op2 `dot` Pure op1 = Pure (op2 . op1)
+        Pure op  `dot` sig = Trans (\x -> op `liftM` runSignal sig x)
+        Trans op `dot` sig = Trans (\x -> runSignal sig x >>= op)
+        Gen g    `dot` sig = Trans (\x -> runSignal sig x >> g)
+        sig'     `dot` sig = Trans (\x -> runSignal sig x >>= runSignal sig')
 
-type IOMC = MC IO
-type SignalSource = IOMC
-type Finalizer = IOMC ()
+instance Arrow (Signal IO) where
+    arr = Pure
+    first Id         = Id
+    first (Pure op)  = Pure (\(a,d) -> (op a, d))
+    first (Trans op) = Trans (\(a,d) -> liftM2 (,) (op a) (return d))
+    first (Loop sig) = Loop $ proc ((a,e),d) -> do (b,d') <- sig -< (a,d)
+                                                   returnA -< ((b,e),d')
+    first (Gen g)    = Trans (\(_,d) -> liftM2 (,) g (return d))
 
-data Signal a = Signal a (Wire a) Finalizer 
+instance ArrowLoop (Signal IO) where
+    loop = Loop
 
-newtype Wire a = Wire [a -> IOMC a]
+runSignal :: Signal IO a b -> a -> IO b
+runSignal sig x 
+  | Id        <- sig = putStrLn "Id" >> return x
+  | Pure op   <- sig = putStrLn "Pure" >> return (op x)
+  | Trans op  <- sig = putStrLn "Trans" >> op x
+  | Gen op    <- sig = putStrLn "Gen" >> op
+  | Loop sig' <- sig = putStrLn "Loop" >> 
+                         (fst `liftM` mfix (\t -> runSignal sig' (x, snd t)))
 
-runSignal :: Signal a -> IOMC a
-runSignal (Signal sourc (Wire wires) fini) = do
-    x <- L.foldr (>=>) return wires sourc
-    fini
-    return x
+p :: Show a => Signal IO a ()
+p = Trans print
 
-source :: a -> Signal a -> Signal a
-source x (Signal _ trans fini) = Signal x trans fini
+t :: (Num a, Random a) => Signal IO a a
+t = Trans $ \x -> do y <- randomRIO (0,100)
+                     return (x+y)
 
-trans :: (a -> IOMC a) -> Signal a -> Signal a
-trans op (Signal ini (Wire ops) fini) = Signal ini (Wire $ ops ++ [op]) fini
-
-signaler :: Int -> Int -> Curs IO (Signal ByteString) (Signal ByteString)
-signaler delayn dropn = (\s -> consum s `liftM` iniW) ^>> act 
-    where 
-    iniW = do 
-        y <- liftIO (randomRIO (0.2,0.8))
-        x <- liftIO (randomRIO (0.2,0.8))
-        w <- newWindow (Height/4) (Width/4) 
-                       (Absolute y * Height) (Absolute x * Width)
-        drawBorder w
-        render
-        return w
-    consum sig win = trans (repl win) sig
-    repl win str = rend win str >> tran win str
-    rend win str = do 
-        erase win
-        drawBorder win
-        moveCursor win 1 1
-        drawByteString win str
-        render
-        liftIO $ threadDelay (delayn * 100000)
-    tran _ str = do 
-        liftIO $ hPrint stderr $ "signaler: " <> str
-        ch <- liftIO (randomRIO ('A','z'))
-        return (BS.drop dropn str <> BS.singleton ch)
-                                          
-m1 :: Curs IO () ByteString               
-m1 = proc () -> do
-    rec os <- signaler 9 2 -< source "string strung" os
-    act -< runSignal os
-
--- input :: Curs IO () (Signal ByteString)
--- input = arr (\() -> foreverSignal getByteString)
--- 
-runCurs :: (MonadIO m, MonadMask m) => Curs m a b -> a -> m b
-runCurs (Curs op) i = runMC $ do
-    runKleisli (runReader op) (i, waitInput)
-
-act :: Monad m => Curs m (MC m output) output
-act = Curs $ (A.lift) (Kleisli id)
-
-actM :: Monad m => Curs m (m output) output
-actM = Curs $ (A.lift) (Kleisli lift)
-
+m1 :: Signal IO Int [Int]
+m1 = proc x -> do
+    p -< x
+    rec y <- t -< x
+        ys <- returnA -< y : (*2) `fmap` ys
+    returnA -< ys
